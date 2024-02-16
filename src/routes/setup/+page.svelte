b09bcb93f4e05072e2b612efbf401f292aae5b33
Mr-Fox-h <mr.fox@iran.ir>
2024-02-15 22:21:34 -1100

    Change: client page

---
+++ src/routes/setup/+page.svelte
@@ -0,0 +1,49 @@
+<section class="bg-background text-etc-3 size-full flex items-center justify-center">
+	<div class="h-screen w-3/6 px-10 py-12">
+		<img src="Images/1.jpg" alt="client" class="size-full object-cover rounded-[50px]" />
+	</div>
+	<div class="h-screen w-screen flex items-center justify-center">
+		<div
+			class="bg-[#29282D] min-h-[700px] max-w-[570px] border-primary-20 border-[1px] rounded-[10px] flex flex-col items-center justify-start p-5"
+		>
+			<div class="flex flex-row items-center justify-center mb-10">
+				<svg class="fill-primary-60 size-6" viewBox="0 0 320 512"
+					><path
+						d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"
+					/></svg
+				>
+				<h2 class="text-3xl px-3">Client</h2>
+				<svg class="fill-primary-60 size-6" viewBox="0 0 320 512"
+					><path
+						d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"
+					/></svg
+				>
+			</div>
+			<div class="min-w-full h-96 flex flex-col items-center">
+				<div class="w-full flex flex-row items-center justify-center my-2 px-7">
+					<p class="mr-3">Server:</p>
+					<input
+						type="text"
+						class="bg-background w-full h-8 border-primary-20 border-[1px] rounded-[10px]"
+					/>
+				</div>
+				<div class="w-full flex flex-row items-center justify-center my-2 px-7">
+					<p class="mr-3">Port:</p>
+					<input
+						type="text"
+						class="bg-background w-full h-8 border-primary-20 border-[1px] rounded-[10px]"
+					/>
+				</div>
+				<button class="bg-primary-40 size-fit rounded-[10px] my-2 py-2 px-6">Start</button>
+			</div>
+			<div class="size-full">
+				<hr class="border-primary-20 border-[1px] rounded-[10px]" />
+				Veniam eiusmod dolore id eu anim ex do non. Aliqua tempor labore tempor fugiat aute dolor quis
+				cillum. Elit veniam excepteur qui laborum commodo Lorem id adipisicing. Consequat magna sunt
+				elit enim velit dolore aliquip. Amet magna velit velit aliquip incididunt in nisi nulla ad consectetur.
+				Incididunt laborum occaecat aute nisi fugiat aute voluptate eu non ipsum culpa pariatur. Aliquip
+				et irure aute officia cillum eiusmod sunt incididunt velit Lorem ipsum consequat magna aliquip.
+			</div>
+		</div>
+	</div>
+</section>
