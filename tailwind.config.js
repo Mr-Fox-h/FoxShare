857a85ee8145ef552cdc11e62f69ddf9e61138ff
Mr-Fox-h <mr.fox@iran.ir>
2024-03-05 20:49:52 -1100

    Change: tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		screens: {
+			xs: '475px',
+			xsMax: { max: '475px' },
+			smMax: { max: '640px' },
+			mdMax: { max: '768px' },
+			lgMax: { max: '1024px' },
+			xlMax: { max: '1280px' },
+			'2xlMax': { max: '1536px' },
+			...defaultTheme.screens
+		},
 		fontFamily: {
 			Inter: ['Inter', 'sans-serif']
 		},
